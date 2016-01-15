ac35b22cdf29856c5b6694a4728cccbe1d05b208
sergeevkc200 <sergeevkc200@mail.ru>
2016-01-15 10:20:01 +0300

    Inet

---
+++ Unit1.pas
@@ -0,0 +1,87 @@
+unit Unit1;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+  Dialogs, Sockets, StdCtrls, ScktComp;
+
+type
+  TForm1 = class(TForm)
+    Button1: TButton;
+    Button2: TButton;
+    Edit1: TEdit;
+    Edit2: TEdit;
+    Edit3: TEdit;
+    ListBox1: TListBox;
+    ClientSocket1: TClientSocket;
+    procedure Button1Click(Sender: TObject);
+    procedure ClientSocket1Connect(Sender: TObject;
+      Socket: TCustomWinSocket);
+    procedure Button2Click(Sender: TObject);
+    procedure ClientSocket1Connecting(Sender: TObject;
+      Socket: TCustomWinSocket);
+    procedure ClientSocket1Disconnect(Sender: TObject;
+      Socket: TCustomWinSocket);
+    procedure ClientSocket1Read(Sender: TObject; Socket: TCustomWinSocket);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.dfm}
+
+procedure TForm1.Button1Click(Sender: TObject);
+begin
+  if ClientSocket1.Active then
+  begin
+  ClientSocket1.Close;
+  Exit;
+  end;
+  try
+  ClientSocket1.Host:=Edit1.Text;
+  ClientSocket1.Port:=StrToInt(Edit2.Text);
+  ClientSocket1.Open;
+  except
+  end;
+end;
+
+procedure TForm1.ClientSocket1Connect(Sender: TObject;
+  Socket: TCustomWinSocket);
+begin
+  showmessage('Соединение установлено');
+  ListBox1.Items.Add('>Hello, i want to play with you in the game');
+  Socket.SendText('Hello, i want to play with you in the game');
+end;
+
+procedure TForm1.Button2Click(Sender: TObject);
+begin
+  ClientSocket1.Socket.SendText(Edit3.Text);
+  ListBox1.Items.Add('>'+Edit3.Text);
+end;
+
+procedure TForm1.ClientSocket1Connecting(Sender: TObject;
+  Socket: TCustomWinSocket);
+begin
+  showmessage('Ожидание соединения');
+end;
+
+procedure TForm1.ClientSocket1Disconnect(Sender: TObject;
+  Socket: TCustomWinSocket);
+begin
+  showmessage('Соединение разорвано');
+end;
+
+procedure TForm1.ClientSocket1Read(Sender: TObject;
+  Socket: TCustomWinSocket);
+begin
+  ListBox1.Items.Add('<'+Socket.ReceiveText);
+end;
+
+end.
---
+++ Unit1.~dfm
@@ -0,0 +1,77 @@
+object Form1: TForm1
+  Left = 192
+  Top = 124
+  Width = 355
+  Height = 389
+  Caption = 'Form1'
+  Color = clBtnFace
+  Font.Charset = DEFAULT_CHARSET
+  Font.Color = clWindowText
+  Font.Height = -11
+  Font.Name = 'MS Sans Serif'
+  Font.Style = []
+  OldCreateOrder = False
+  PixelsPerInch = 96
+  TextHeight = 13
+  object Button1: TButton
+    Left = 24
+    Top = 0
+    Width = 97
+    Height = 49
+    Caption = 'connect'
+    TabOrder = 0
+    OnClick = Button1Click
+  end
+  object Button2: TButton
+    Left = 24
+    Top = 64
+    Width = 65
+    Height = 49
+    Caption = 'send'
+    TabOrder = 1
+    OnClick = Button2Click
+  end
+  object Edit1: TEdit
+    Left = 136
+    Top = 0
+    Width = 193
+    Height = 21
+    TabOrder = 2
+    Text = 'Edit1'
+  end
+  object Edit2: TEdit
+    Left = 136
+    Top = 32
+    Width = 193
+    Height = 21
+    TabOrder = 3
+    Text = 'Edit2'
+  end
+  object Edit3: TEdit
+    Left = 136
+    Top = 88
+    Width = 193
+    Height = 21
+    TabOrder = 4
+    Text = 'Edit3'
+  end
+  object ListBox1: TListBox
+    Left = 24
+    Top = 120
+    Width = 305
+    Height = 209
+    ItemHeight = 13
+    TabOrder = 5
+  end
+  object ClientSocket1: TClientSocket
+    Active = False
+    ClientType = ctNonBlocking
+    Port = 0
+    OnConnecting = ClientSocket1Connecting
+    OnConnect = ClientSocket1Connect
+    OnDisconnect = ClientSocket1Disconnect
+    OnRead = ClientSocket1Read
+    Left = 96
+    Top = 88
+  end
+end
